df2541738fa9d0390e86055748b47259ff8e36c8
Nadia-Hamid <com.nadiahamid>
2022-02-02 15:00:47 +0100

    Criando repository e service para Category

--- src/main/java/com/nadiahamid/cursomc/resources/CategoryResource.java
+++ src/main/java/com/nadiahamid/cursomc/resources/CategoryResource.java
@@ -1,27 +1,25 @@
 package com.nadiahamid.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nadiahamid.cursomc.domain.Category;
+import com.nadiahamid.cursomc.service.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Category> list() {
-		Category cat1 = new Category(1, "Informática");
-		Category cat2 = new Category(1, "Escritório");
+	@Autowired
+	private CategoryService service;
 
-		List<Category> list = new ArrayList<>();
-		list.add(cat1);
-		list.add(cat2);
-
-		return list;
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Category obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/nadiahamid/cursomc/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.nadiahamid.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nadiahamid.cursomc.domain.Category;
+import com.nadiahamid.cursomc.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repo;
+
+	public Category find(Integer id) {
+		Optional<Category> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
